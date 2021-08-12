b7c6c61355a9653477edecaad99284b09a5fb1b3
knwg298n6 <88834842+knwg298n6@users.noreply.github.com>
2021-08-12 18:04:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app
+until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app --advertise-exit-node
 do
     sleep 0.1
 done
